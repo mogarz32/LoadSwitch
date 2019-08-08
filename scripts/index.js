990e6ac63e98d4bf10db97a497e2326b266b8671
Zach Ressler <sockoman32@aol.com>
2019-08-08 19:05:52 -0400

    Changes background image.

--- scripts/index.js
+++ scripts/index.js
@@ -1322,9 +1322,11 @@ function syncMenu1() {
 	var selectedGroup = 0;
 	if(selection < 13) {	// Awakening - 13 tracks
 		selectedGroup = 0;
+		setBackground("url(../assets/awakening-logo.png)");
 	} else if(selection < 42) {	// Fates - 29 tracks
 		selection = selection - 13;
 		selectedGroup = 1;
+		setBackground("url(../assets/fates-logo.png)");
 	} else if(selection < 47) {	// Darkest Dungeon Battle - 5 tracks
 		selection = selection - 42;
 		selectedGroup = 2;
@@ -1366,9 +1368,11 @@ function syncMenu2() {
 	var selectedGroup = 0;
 	if(selection < 13) {	// Awakening Ablaze - 13 tracks
 		selectedGroup = 0;
+		setBackground("url(../assets/awakening-logo.png)");
 	} else if(selection < 42) {	// Fates Ablaze - 29 tracks
 		selection = selection - 13;
 		selectedGroup = 1;
+		setBackground("url(../assets/fates-logo.png)");
 	} else if(selection < 47) {	// Darkest Dungeon Ambush - 5 tracks
 		selection = selection - 42;
 		selectedGroup = 2;
@@ -1481,6 +1485,11 @@ function check() {
     document.getElementById('img').className = 'img2';
     document.getElementById('volume').className = 'volume2';
   }
+}
+
+// Background image changing
+function setBackground(imageURL) {
+	document.getElementsByClassName("container").style.backgroundImage = imageURL;
 }
 
 // Textfield URL video queueing
